de6794d9964716809c8744c9c8a23d46187ffa44
Megan McCartney <meganmccartney@Megans-Air.localdomain>
2022-11-29 00:41:32 -0500

    added prompts

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//Prompt User for password criteria
+var length = prompt("Your password must be between 8-128 characters. How many characters would you like your password to be?");
+var yes = ["y","n"];
+var uppercase = prompt("Do you want to add Uppercase characters?", yes);
+var lowercase = prompt("Do you want to add lowercase?", yes);
+var numbers = prompt("Do you want to add numbers?", yes);
+var special = prompt("Do you want to add special characters?", yes);
+
+
+
+function generatePassword() {
+    var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 30
+    var passGen = "";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * characters.length);
+        passGen += characters.substring(randomNumber, randomNumber +1);
+    }
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
