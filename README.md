76d373a9e16e9260d0e7fa9bc55762c1ce742a0d
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-04-20 16:27:40 -0400

    rework of the password code to get it to work

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Flower-Power
-repository for week 3 JavaScript Challenge
+On this page, you may generate a random password.
+
+First we set the generate password button to a variable.
+
+Next we are creating a function to do the actual generating of the random characters that will make up the new password.  We add a quick console.log to make sure the event listener further down in the code is working.  Then we set up prompts for the user to input their desired password characteristics the function will utilize.
+
+Then we set the character variables that the function will be picking at random.
+We create a for loop to run through those characters using the math.random operator to make the random choices.
+We return the chosen values.
+
+We also create a function to write the password. We put the generatePassword() function within this fuction.  Set a variable for the passwordText and call the returned password value which will display the new password to the page.
+
+Lastly we add an eventListener to listen for the click on the Generate Password button.  This event will kickstart the rest of our code.
+
--- script.js
+++ script.js
@@ -1,60 +1,36 @@
-//Prompt User for password criteria
-var uppercase = prompt("Include Uppercase characters?"); {
-  ans = confirm('Yes');
-  if (confirm) {
-    //if yes include Uppercase characters
-  } else {
-    //if no don't include Uppercase characters
-  }
-}
-var lowercase = prompt("Include lowercase characters?");{
-  ans = confirm('Yes');
-  if (ans = "OK") {
-    //if yes include lowercase characters
-  } else {
-    //if no don't include lowercase characters
+//setting the variable for the Generate Password Button.
+var generateBtn = document.querySelector('#generate');
 
-  }
-}
-var numbers = prompt(" Include numbers?");{
-  ans = confirm('Yes');
-  if (ans) {
-    //if yes include numbers
-  } else {
-    //if no don't include numbers
-  }
-}
-var special = prompt("Include special characters?"); {
-  ans = confirm('Yes');
-  if (ans) {
-    //if yes include special characters
-  } else {
-    //if no don't include special characters
-  }
-}
-
-
-
+//creating the function to generate the random password
 function generatePassword() {
-    var alphaLow = "abcdefghijklmnopqrstuvwxyz";
-    var alphaUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numerics = "01234567890"
-    var symbols = "!@#$%^&*()_-+="
-    
+  //making sure the event listener for the button is working
+  console.log("Thank you, your password is being generated...");
+  //prompting the user for their desired password characteristics.
+  var passwordLength = prompt("How many characters would you like your password to contain?");
+  prompt("Please click OK to confirm special characters.");
+  prompt("Please click OK to confirm number characters.");
+  prompt("Please click OK to confirm lowercase characters.");
+  prompt("Please click OK to confirm uppercase characters.");
+
+  //setting the character variables
+  var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //creating a for loop to loop through all possible characters, and using math.random to randomize the chosen characters
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+   };
+
+  return password;
 }
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+//Function to write the password on the page
 function writePassword() {
+  //the function generatePassword is being called and the value is being stored in the variable 'password'.
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  var passwordText = document.querySelector('#password');
+  //This displays the password onto the page
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// Add event listener to the generate password button
 generateBtn.addEventListener("click", writePassword);
